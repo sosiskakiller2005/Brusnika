42736a4c57107d45fe766867c4233df4cfed0c27
Влад Козлов <vkozlov15012005@gmail.com>
2024-12-09 17:17:11 +0300

    Добавьте файлы проекта.

---
+++ Brusnika/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Brusnika.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Brusnika
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static BrusnikaDbEntities _context;
+        public static BrusnikaDbEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new BrusnikaDbEntities();
+            }
+            return _context;
+        }
+    }
+}
